8e4bee01bc194ec5423de72d0334a8f23cb8dc88
CME <codemastereduardo@gmail.com>
2023-11-02 02:11:36 -0400

    exposed web server to "0.0.0.0"

--- src/frontend/vite.config.ts
+++ src/frontend/vite.config.ts
@@ -5,9 +5,8 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  server: { host: '0.0.0.0', port: 80 },
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
